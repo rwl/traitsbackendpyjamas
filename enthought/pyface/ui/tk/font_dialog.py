b6d1112a12c4156258274460c8ccdb33e0f0069c
Richard Lincoln <r.w.lincoln@gmail.com>
2009-02-02 16:01:20 +0000

    Added list box to font dialog.

--- enthought/pyface/ui/tk/font_dialog.py
+++ enthought/pyface/ui/tk/font_dialog.py
@@ -31,21 +31,48 @@ import Tix \
 class FontDialog(tkSimpleDialog.Dialog):
     """ A toolkit specific dialog for font selection. """
 
-    def body(self, master):
+    def __init__(self, parent, title="Choose Font"):
+        """ Initialises the font dialog.
+        """
+        tkSimpleDialog.Dialog.__init__(self, parent, title)
+
+
+
+    def body(self, frame):
         """ Creates the dialog body. Returns the widget that should have
             initial focus.
         """
-        title = tk.Label( master, text = self.title )
-        title.pack()
+        master = tk.Frame(self)
+        master.pack(padx=5, pady=0, expand=1, fill=tk.BOTH )
+
+        title = tk.Label( master, text = "Choose Font" )
+        title.pack(side=tix.TOP)
+#        title.grid( row = 0, columnspan = 2 )
 
         # Font family list ----------------------------------------------------
 
+        font_frame = tk.Frame( master )
+        font_frame.pack(side=tk.TOP, expand=1, fill=tk.BOTH)
+
         family_var = tix.StringVar()
-        family     = tix.ComboBox( master,
-#                                   label = "Font family", labelside = 'top',
-                                   dropdown = 0, variable = family_var,
-                                   options = "label.anchor e" )
-        family.pack()
+#        family     = tix.ComboBox( master,
+##                                   label = "Font family", labelside = 'top',
+#                                   dropdown = 0, variable = family_var,
+#                                   options = "label.anchor e" )
+
+        family = tk.Listbox( font_frame,
+                             selectmode     = tk.SINGLE,
+#                             yscrollcommand = scrollbar.set
+                             bg             = "White" )
+
+        scrollbar = tk.Scrollbar( family, orient = tk.VERTICAL )
+        family.config( yscrollcommand = scrollbar.set, height=200 )
+
+        scrollbar.config( command = family.yview )
+        scrollbar.pack( side = tk.RIGHT, fill = tk.Y )
+
+        family.pack( side = tix.LEFT, expand = 1, fill = tk.BOTH, anchor = tk.S )
+#        family.grid( row = 1, column = 0, sticky="nsew" )
 
         for family_name in families(master):
             family.insert( tix.END, family_name )
@@ -55,17 +82,22 @@ class FontDialog(tkSimpleDialog.Dialog):
         # Font size spinner ---------------------------------------------------
 
         size_var = tix.IntVar()
-        size     = tix.Control( master,
+
+        size     = tix.Control( font_frame,
 #                                label = "Font Size", labelside = 'top',
                                 integer = 1, min = 2, max = 80,
                                 variable = size_var,
                                 options = "label.anchor e" )
-        size.pack()
+
+        size.pack( side = tix.RIGHT, anchor = tk.N )
+#        size.grid( row = 1, column = 1, sticky=tk.E )
 
         # Example text --------------------------------------------------------
 
-        example = tix.Text( master, height=3 )
-        example.pack(expand=1, fill=tix.X)
+        example = tix.Text( master, height=3, width = 52, bg = "White" )
+        example.pack(side=tk.TOP, fill=tix.X, anchor=tk.N)
+#        example.grid( row = 2, columnspan = 2 )
+
         example.tag_config( "center", justify = tix.CENTER )
         example.insert(tix.END, "This is example text.\n", "center")
         example.insert(tix.END,
@@ -74,8 +106,10 @@ class FontDialog(tkSimpleDialog.Dialog):
         def on_select( event ):
             example.config( font = ( family_var, size_var.get() ) )
 
-        family.config( command = on_select )
-        family.config( command = on_select )
+#        family.config( command = on_select )
+#        family.config( command = on_select )
+
+        return size # Given initial focus.
 
 
     def buttonbox(self):
@@ -93,7 +127,7 @@ class FontDialog(tkSimpleDialog.Dialog):
         self.bind("<Return>", self.ok)
         self.bind("<Escape>", self.cancel)
 
-        box.pack(side=tix.RIGHT, padx=5, pady=5)
+        box.pack(side=tix.RIGHT, padx=5, pady=5, anchor=tk.S)
 
 
     def validate(self):
