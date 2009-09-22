8a681b26be96bd221c22ff1c5900f39176f554ea
Richard W. Lincoln <r.w.lincoln@gmail.com>
2009-09-22 11:11:24 +0100

    Removing "W.".

--- enthought/pyface/__init__.py
+++ enthought/pyface/__init__.py
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------
-#  Copyright (c) 2009 Richard W. Lincoln
+#  Copyright (c) 2009 Richard Lincoln
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to
